0e995715bb6021054f98d6646f16402739d88dc7
ifejayeola <ijjayeola@gmail.com>
2025-07-03 13:11:47 +0100

    Attempt 1 on task 0'

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,15 @@
+SELECT Booking.booking_id, "User".first_name, "User".last_name
+FROM Booking
+INNER JOIN "User"
+ON Booking.user_id = "User".user_id;
+
+
+SELECT Property.description, Property.location, Review.rating, Review.comment
+FROM Property
+LEFT JOIN Review
+ON Property.property_id = Review.property_id;
+
+SELECT first_name, last_name, start_date, end_date, total_price, status booking_status
+FROM "User"
+LEFT JOIN Booking
+ON "User".user_id = Booking.user_id;
--- database-script-0x02/seed.sql
+++ database-script-0x02/seed.sql
@@ -1,31 +1,33 @@
 
 
+
 -- To create a new users
 INSERT INTO "User" (user_id, first_name, last_name, email, password_hash, phone_number, role, created_at)
-VALUES (DEFAULT, 'Ade', 'Kogbe', 'fmynewmail@gmail.com', '$2b$12$eWliD5PBmyNnWp3OKjbe3O8.5Il1HZXYWWT6cMKnIRcGbFsnW0.Qq
-', '0809797977', 'guest', DEFAULT)
+VALUES (DEFAULT, 'Ade', 'Kogbe', 'fhmynewmail@gmail.com', '$2b$12$eWliD5PBmyNnWp3OKjbe3O8.5Il1HZXYWWT6cMKnIRcGbFsnW0.Qq
+', '0809797977', 'guest', DEFAULT) RETURNING user_id;
+
 
 -- To create new property
 INSERT INTO Property (property_id, host_id, name, description, location, pricepernight, created_at, updated_at)
-VALUES ( DEFAULT, '550e8400-e29b-41d4-a716-446655440000', 'Duplex', 'Semi-detached', 'Aule', 100.89, DEFAULT, DEFAULT
-);
+VALUES ( DEFAULT, (SELECT user_id FROM "User"), 'Duplex', 'Semi-detached', 'Aule', 100.89, DEFAULT, DEFAULT
+) RETURNING property_id;
 
--- To create new Booking
 INSERT INTO Booking(booking_id, property_id,  user_id, start_date, end_date, total_price, status, created_at)
 VALUES
-( DEFAULT, '123e4567-e89b-12d3-a456-426614174000', '987e6543-e21a-45b3-b789-124567890abc', '2025-07-01', '2025-07-05', 400.00, 'pending', DEFAULT),
-( DEFAULT, '123e4567-e89b-12d3-a456-426614174000', '987e6543-e21a-45b3-b789-124567890abc', '2025-08-10', '2025-08-15', 550.00, 'confirmed', DEFAULT);
-
+( DEFAULT,'8ad9ef49-e137-4ad7-ac6f-91376e9f831a', 'e2c4c4e7-4dd7-4914-b9ae-f8247e77cac4','2025-08-10', '2025-08-15', 550.00, 'confirmed', DEFAULT);
 
 --To create for Pyament
 INSERT INTO Payment ( payment_id, booking_id, amount, payment_date, payment_method)
-VALUES (DEFAULT, '987e6543-e21a-45b3-b789-124567890abc', 4000, '2025-08-10', 'paypal');
-
+VALUES (DEFAULT, '0b478b32-28fd-4dbc-bcc9-b13f8dd507f1', 4000, '2025-08-10', 'paypal');
 
 -- Creating review
 INSERT INTO Review ( review_id, property_id, user_id, rating, comment, created_at)
-VALUES (DEFAULT, '987e6543-e21a-45b3-b789-124567890abc', '87e6543-e21a-45b3-b789-124567890abc', 3, 'This is the best', DEFAULT);
+VALUES (DEFAULT, '8ad9ef49-e137-4ad7-ac6f-91376e9f831a', 'e2c4c4e7-4dd7-4914-b9ae-f8247e77cac4', 3, 'This is the best', DEFAULT);
 
 -- Adding a message
 INSERT INTO Message ( message_id, sender_id, recipient_id, message_body, sent_at)
-VALUES (DEFAULT, '7e6543-e21a-45b3-b789-124567890abc', '23e4567-e89b-12d3-a456-4266141740', 'i LIKE YOUR PRODUCT', DEFAULT);
+VALUES (DEFAULT, 'e2c4c4e7-4dd7-4914-b9ae-f8247e77cac4', '1b33fe69-d96a-421a-b767-e72f5800e3e3', 'i LIKE YOUR PRODUCT', DEFAULT);
+
+
+-----------------------------------------------------------------------------------------------------------------------------------
+
