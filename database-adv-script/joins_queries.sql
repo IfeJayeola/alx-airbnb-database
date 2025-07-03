6e9654fb0aff2c50a97c4ed23af4d0198a21b92d
ifejayeola <ijjayeola@gmail.com>
2025-07-03 13:31:26 +0100

    Attempt 2 on task 0'- ORDER BY

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -7,9 +7,10 @@ ON Booking.user_id = "User".user_id;
 SELECT Property.description, Property.location, Review.rating, Review.comment
 FROM Property
 LEFT JOIN Review
-ON Property.property_id = Review.property_id;
+ON Property.property_id = Review.property_id
+ORDER BY location
 
 SELECT first_name, last_name, start_date, end_date, total_price, status booking_status
 FROM "User"
-LEFT JOIN Booking
+FULL OUTER JOIN Booking
 ON "User".user_id = Booking.user_id;
