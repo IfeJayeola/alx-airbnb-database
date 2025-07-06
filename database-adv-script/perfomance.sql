2256b1f34749e6f1d0815705eef2ebd09b4355e4
IfeJayeola <ijjayeola@gmail.com>
2025-07-06 22:04:54 +0100

    Update perfomance.sql checker

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,5 +1,20 @@
-SELECT start_date, end_date, total_price, status, description, location, payment_date, payment_method
+
+EXPLAIN
+SELECT 
+    Booking.start_date,
+    Booking.end_date,
+    Booking.total_price,
+    Booking.status,
+    "User".first_name,
+    "User".last_name,
+    "User".email,
+    Property.description,
+    Property.location,
+    Payment.payment_date,
+    Payment.payment_method
 FROM Booking
 LEFT JOIN "User" ON Booking.user_id = "User".user_id
 LEFT JOIN Property ON Booking.property_id = Property.property_id
-LEFT JOIN Payment ON Booking.booking_id = Payment.booking_id;
+LEFT JOIN Payment ON Booking.booking_id = Payment.booking_id
+WHERE Booking.status = 'confirmed'
+  AND Payment.status = 'completed';
